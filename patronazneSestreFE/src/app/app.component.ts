b762a8bafc4a59c545ec732facbf7772d9271329
drejcar <drejcar@gmail.com>
2017-04-11 12:05:04 +0200

    neki

--- patronazneSestreFE/src/app/app.component.ts
+++ patronazneSestreFE/src/app/app.component.ts
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
   <h1>Hello motherfucking {{name}}</h1>
   <nav>
     <a routerLink="/dashboard" routerLinkActive="active">Domov</a>
+    <a routerLink="/dashboard" routerLinkActive="active">Prijava</a>
+    <a routerLink="/dashboard" routerLinkActive="active">Registracija</a>
   </nav>
   <router-outlet></router-outlet>
   `,
