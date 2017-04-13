a3023e3416a43ec73e2511b268286cf9df11ce34
drejcar <drejcar@gmail.com>
2017-04-13 23:56:01 +0200

    poskus na 404 strani

--- patronazneSestreFE/src/app/not-found.component.ts
+++ patronazneSestreFE/src/app/not-found.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector:'not-found',
   template: `<h2>This is not the page you are looking for</h2>
              <p>Ups, izgleda da ste zašli na neveljavno oziroma nedostopno stran</p>
-            `
+            `,
+  styleUrls:[]
 })
 export class PageNotFoundComponent {}
