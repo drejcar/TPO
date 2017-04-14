3d03f36dcb30299161e8575a98b802b641792e5e
drejcar <drejcar@gmail.com>
2017-04-14 11:48:48 +0200

    validacija regzd in naloga

--- patronazneSestreFE/src/app/ZdravstveniDelavec.ts
+++ patronazneSestreFE/src/app/ZdravstveniDelavec.ts
@@ -2,7 +2,8 @@ export class ZdravstveniDelavec{
   constructor(
     public ime: String,
     public priimek :String,
-    public sifraZd:String,   
+    public sifraZd:String,
+    public tel:String,
     public uporabnik: UporabnikZd
   ){}
 }
--- patronazneSestreFE/src/app/registracija_zd/registracija_zd.component.ts
+++ patronazneSestreFE/src/app/registracija_zd/registracija_zd.component.ts
@@ -28,10 +28,11 @@ export class Registracija_zdComponent {
   pwd='';
   sifraZd='';
   idvloga=3;
+  tel='';
 
   model3 = new Vloga(this.idvloga);
   model2 = new UporabnikZd(this.mail,this.pwd,this.model3);
-  model=new ZdravstveniDelavec(this.ime,this.priimek,this.sifraZd,this.model2);
+  model=new ZdravstveniDelavec(this.ime,this.priimek,this.sifraZd,this.tel,this.model2);
   submitted=false;
   onSubmit(){this.submitted=true;}
   novVloga(){
@@ -41,7 +42,7 @@ export class Registracija_zdComponent {
 	  this.model2 = new UporabnikZd(this.mail,this.pwd,this.model3);
   }
   novZd(){
-	this.model=new ZdravstveniDelavec(this.ime,this.priimek,this.sifraZd,this.model2);
+	this.model=new ZdravstveniDelavec(this.ime,this.priimek,this.sifraZd,this.tel,this.model2);
   }
   //ne potrebujes
   get diagnostic() { return JSON.stringify(this.model); }
