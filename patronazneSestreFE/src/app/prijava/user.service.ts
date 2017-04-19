6262e3fdeb626982615c70ef8d9fdbdf65597bad
drejcar <drejcar@gmail.com>
2017-04-19 11:36:04 +0200

    tmp

--- patronazneSestreFE/src/app/prijava/user.service.ts
+++ patronazneSestreFE/src/app/prijava/user.service.ts
@@ -11,56 +11,56 @@ import 'rxjs/add/operator/catch'
 
 @Injectable()
 export class UserService {
- 
+
  private headers = new Headers({'Content-Type': 'application/json','Authorization':'Basic ' + btoa('guest@guest:guest')});
   private loggedIn = false;
   private baseUrl: String = 'http://localhost:8080/patronazneSestre/v1';
   upr: Upr;
   constructor(private http: Http) {
     this.loggedIn = !!localStorage.getItem('username');
-	
+
   }
 
   login(prijava:Prijava): Observable<Upr> {
-		
+
 		return this.http.get(`${this.baseUrl}/uporabnik/login/${prijava.mail}`, {headers: this.headers}).map((res) => {return this.mapUporabnik(res)});
-		
+
 		//localStorage.setItem('username', this.result.email);
 		//localStorage.setItem('password', this.result.geslo);
-		
+
   }
 
-  
-	  
-	  
-	  
-	  
- 
+
+
+
+
+
+
   //funkcija za ustvarjanje avtorizacijski header
   createAuthorizationHeader(headers:Headers, pass: String, username: String){
 	headers.append('Authorization', 'Basic' + btoa(username+':'+pass));
 	return headers;
  }
- 
+
  mapUporabnik(response: Response): Upr{
   return this.toUporabnik(response.json());
 }
 //pretvorba json objekta v angular objekt
  toUporabnik(r:any): Upr{
   let vlog = <Vloga>({
-	 idVloga: r.vloga.idvloga; 
-	 opis: r.vloga.opis	
+	 idVloga: r.vloga.idvloga,
+	 opis: r.vloga.opis
   });
-  let uporabnik = <Upr>({ 	 
+  let uporabnik = <Upr>({
 	 email: r.email,
 	 geslo: r.geslo,
 	 vloga: r.vloga,
   });
-  
+
   return uporabnik;
- }  
+ }
   logout() {
-    
+
 	localStorage.removeItem('username');
 	localStorage.removeItem('password');
 	localStorage.removeItem('vloga');
