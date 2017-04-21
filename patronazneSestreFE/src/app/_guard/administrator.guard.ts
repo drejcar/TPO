7359ea7f142f63c32e680424ab3e3191b3a56306
drejcar <drejcar@gmail.com>
2017-04-21 10:13:18 +0200

    dopolnitev guardov

--- patronazneSestreFE/src/app/_guard/administrator.guard.ts
+++ patronazneSestreFE/src/app/_guard/administrator.guard.ts
@@ -7,6 +7,6 @@ export class AdministratorGuard implements CanActivate {
   constructor(private user: UserService) {}
 
   canActivate() {
-    return this.user.isLoggedIn();
+    return (this.user.isLoggedIn() && localStorage.getItem("vloga")=="Administrator");
   }
 }
--- patronazneSestreFE/src/app/_guard/sodelavecizs.guard.ts
+++ patronazneSestreFE/src/app/_guard/sodelavecizs.guard.ts
@@ -7,6 +7,6 @@ export class SodelavecIZSGuard implements CanActivate {
   constructor(private user: UserService) {}
 
   canActivate() {
-    return this.user.isLoggedIn();
+    return (this.user.isLoggedIn() && localStorage.getItem("vloga")=="SodelavecIZS");
   }
 }
