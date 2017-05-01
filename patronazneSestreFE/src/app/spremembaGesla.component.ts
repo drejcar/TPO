324c7ff8e35715b335032d1b778a6350802721fb
CheBato <matevz.ogrinc@hotmail.com>
2017-05-01 23:19:42 +0200

    popravljeno sprememba gesla

--- patronazneSestreFE/src/app/spremembaGesla.component.ts
+++ patronazneSestreFE/src/app/spremembaGesla.component.ts
@@ -23,7 +23,7 @@ export class SpremembaGeslaComponent{
 
   pwd:String='';
   pwd2:String='';
-  model : Object = ({pwd: this.pwd, pwd2: this.pwd2});
+  model = ({'pwd': this.pwd, 'pwd2': this.pwd2});
   submitted=false;
   fail= false;
   onSubmit(){
@@ -37,7 +37,7 @@ export class SpremembaGeslaComponent{
 	let upr = <Upr>({
 		iduporabnik: Number(localStorage.getItem('iduporabnik')),
 		email: localStorage.getItem('email'),
-		geslo: this.pwd,
+		geslo: this.model.pwd,
 		vloga: vlog,
 		zadnjaPrijava: localStorage.getItem('datumZadnjePrijave'),
 	});
