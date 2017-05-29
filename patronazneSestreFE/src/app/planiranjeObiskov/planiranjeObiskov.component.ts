5123dc83d5916dcd4e987a30f2490fd4a30a83f6
CheBato <matevz.ogrinc@hotmail.com>
2017-05-29 10:56:04 +0200

    deluje nadomeščanje

--- patronazneSestreFE/src/app/planiranjeObiskov/planiranjeObiskov.component.ts
+++ patronazneSestreFE/src/app/planiranjeObiskov/planiranjeObiskov.component.ts
@@ -53,7 +53,9 @@ export class PlaniranjeObiskovComponent implements OnInit{
 					var datum = this.izbraniDatum;
 					for(let dn of this.tabelaObiskovVsi){
 						for(let ob of dn.obisks){
-							if(ob.nadomestnaSestra != null){
+							console.log(ob.nadomestnaSestra.idzdravstveniDelavec);
+							console.log(localStorage.getItem('idZdravstvenegaDelavca'));
+							if(ob.nadomestnaSestra.idzdravstveniDelavec != localStorage.getItem('idZdravstvenegaDelavca')){
 								continue;
 							}
 							let obisk = <any> ({idObiska:0,izdajatelj:'',vrstaObiska:'',patronaznaSestra:'',pacienti:'',predvideniDatumObiska:'',dejanskiDatumObiska:'',opravljenost:'',dodaj:'',fiksniDatum:'',idDelovniNalog:0,nadomescanje:''});
