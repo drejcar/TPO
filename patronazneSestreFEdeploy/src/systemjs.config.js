b370a56ecae1d4c70731b466d6ac562952dfe67d
drejcar <drejcar@gmail.com>
2017-06-06 14:22:10 +0200

    sysconfig

--- patronazneSestreFEdeploy/src/systemjs.config.js
+++ patronazneSestreFEdeploy/src/systemjs.config.js
@@ -27,6 +27,7 @@
       'rxjs':                      'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
       'ng2-validation': 'npm:ng2-validation/bundles/ng2-validation.umd.js'
+	  'libphonenumber-js':'npm:libphonenumber-js/bundle'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -39,6 +40,10 @@
         }
       },
       rxjs: {
+        defaultExtension: 'js'
+      },
+	   'libphonenumber-js': {
+        main: 'libphonenumber-js.min',
         defaultExtension: 'js'
       }
     }
